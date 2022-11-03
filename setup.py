f279dfd9f8185196f4d2f32ab1e2f8b9a2790fef
bleumink <bleumink@gmail.com>
2022-11-03 12:19:07 +0100

    version bumb

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="streamlit-keycloak-component",
-    version="0.1.0",
+    version="0.1.1",
     author="",
     author_email="",
     description="Keycloak authentication for Streamlit",
